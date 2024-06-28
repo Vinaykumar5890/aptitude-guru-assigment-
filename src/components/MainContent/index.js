df850a3abf89bfcde2307fe68010997124b526f0
vinay <devaragarivinayyadav7@gmail.com>
2024-06-28 07:37:17 +0000

    solution

---
+++ src/components/MainContent/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Schedule from '../Schedule'
+import './index.css'
+
+function MainContent() {
+  return (
+    <div className="main-content">
+      <Schedule />
+    </div>
+  )
+}
+
+export default MainContent
